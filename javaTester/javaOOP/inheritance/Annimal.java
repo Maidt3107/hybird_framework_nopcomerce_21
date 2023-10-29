ac49760c5e1ce201c70a1884551a96a1de9ab017
DELL <DELL@DESKTOP-LF7N452>
2023-10-29 22:26:29 +0700

    Topic 65 (Java OOP - Part VII: Inheritance and Encapsulation)

---
+++ javaTester/javaOOP/inheritance/Annimal.java
@@ -0,0 +1,8 @@
+package javaOOP.inheritance;
+
+public class Annimal {
+	protected void eat() {
+		System.out.println("Eating...");
+	}
+
+}
---
+++ javaTester/javaOOP/inheritance/Test.java
@@ -0,0 +1,21 @@
+package javaOOP.inheritance;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test {
+	
+
+	public static void main(String[] args) {
+		Dog dog = new Dog();
+		
+		dog.setAge(-15); 
+		System.out.println(dog.age);
+		
+		dog.setAge(15); 
+		System.out.println(dog.age);
+		
+		dog.setAge(0); 
+		System.out.println(dog.age);
+	}
+ 
+}
